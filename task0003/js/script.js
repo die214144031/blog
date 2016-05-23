f3b764161fa5d3c01387975bf5f4a6f8cff501d5
die214144031 <772853078@qq.com>
2016-05-23 19:44:36 +0800

    xiugai

--- task0003/js/script.js
+++ task0003/js/script.js
@@ -1,49 +1,48 @@
-    window.onload = function() {
-    	console.log("网页可见区域宽"+document.body.clientWidth);
-    	console.log("网页可见区域高"+document.body.clientHeight);
-    	console.log("网页可见区域宽（包括边线宽）"+document.body.offsetWidth);
-    	console.log("网页可见区域高（包括边线高）"+document.body.offsetHeight);
-    	console.log("网页正文全文宽"+document.body.scrollWidth);
-    	console.log("网页正文全文高"+document.body.scrollHeight);
-    	console.log("网页被卷去高"+document.body.scrollTop);
-    	console.log("网页被卷去左"+document.body.scrollLeft);
-    	console.log("网页正文部分上"+window.screenTop);
-    	console.log("网页正文部分左"+window.screenLeft);
-    	console.log("屏幕分辨率高"+window.screen.height);
-    	console.log("屏幕分辨率宽"+window.screen.width);
-    	console.log("屏幕可用工作区宽"+window.screen.availWidth);
-    	console.log("屏幕可见工作区高"+window.screen.availHeight);
+window.onload = function() {
+	var page1 = document.getElementById("page1");
+	var page2 = document.getElementById("page2");
+	var page3 = document.getElementById("page3");
 
-    	var music = document.getElementById('music');
-    	var audio = document.getElementsByTagName('audio')[0];
-    	var page1 = document.getElementById('page1');
-    	var page2 = document.getElementById('page2');
-    	var page3 = document.getElementById('page3');
-    	var p1_lantern=document.getElementsByClassName('p1_lantern')[0];
-    	audio.addEventListener("ended", function(e) {
-    		this.setAttribute('class', "");
-    	}, false);
+	var music = document.getElementById("music");
+	var audio = document.getElementsByTagName("audio")[0];
+	//音乐播放停止，光盘停止
+	audio.addEventListener("ended", function(event) {
+		music.setAttribute("class", "");
+	}, false);
 
-    	music.addEventListener('touchstart', function(e) {
-    		if (audio.paused) {
-    			audio.play();
-    			this.setAttribute("class", "play");
-    		} else {
-    			audio.pause();
-    			this.setAttribute("class", "");
-    		}
-    	}, false);
+	//点击音乐 ，控制播放
+	//music.onclick = function() {
+	//		if (audio.paused) {
+	//			audio.play();
+	//			this.setAttribute("class","play");
+	//		} else {
+	//			audio.pause();
+	//		this.setAttribute("class","");
+	//		};
+	//	};
+	music.addEventListener("touchstart", function(event) {
+		if (audio.paused) {
+			audio.play();
+			this.setAttribute("class", "play");
+		} else {
+			audio.pause();
+			this.setAttribute("class", "");
+		};
+	}, false);
 
-    	p1_lantern.addEventListener('touchstart',function(e){
-    		page1.style.display="none";
-    		page2.style.display="block";
-    		page3.style.display='block';
-    		page3.style.top="100%";
 
-    		setTimeout(function(){
-    		page2.setAttribute('class','page fadeOut');
-    		page3.setAttribute('class','page fadeIn');
-    	},5500);
-    	},false);
 
-   }
+    page1.addEventListener("touchstart", function(event) {
+	page1.style.display = "none";
+	page2.style.display = "block";
+	page3.style.display = "block";
+	page3.style.top =     "100%";
+
+	setTimeout(function(){
+		page2.setAttribute("class","page fadeOut");
+		page3.setAttribute("class","page fadeIn");
+	},5500);
+}, false);
+
+
+};
